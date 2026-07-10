b0e8d1d790430120ae4a20aa4ff99ae59cc18452
RahmiW <abdulrahmanwardere@gmail.com>
2026-07-09 23:36:02 -0500

    Removed unused import in CommentService

--- src/main/java/org/example/megahottakes/services/CommentService.java
+++ src/main/java/org/example/megahottakes/services/CommentService.java
@@ -10,7 +10,6 @@ import org.example.megahottakes.repositories.HotTakeRepository;
 import org.example.megahottakes.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
